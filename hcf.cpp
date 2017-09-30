7336009f2647b8eb3f12f4f10fa001b6da437447
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-09-30 21:23:27 +0530

    Update hcf.cpp

--- hcf.cpp
+++ hcf.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+using namespace std;
+int main()
+{ int a,b;
+cin>>a>>b;
+if(a>b)
+ { for(int n=b;n>=1;n--)
+   {if(a%n==0 && b%n==0)
+     {cout<<n;
+     break;}
+   }
+ }
+else
+
+ { for(int n=a;n>=1;n--)
+   {if(a%n==0 && b%n==0)
+     {cout<<n;
+     break;}
+   }
+ }
+        return 0;
+}
