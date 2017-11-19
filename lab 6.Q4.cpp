4b5141b600450bd56ab81c314cfd5da58a103f8e
Rimilmandrita <31147874+Rimilmandrita@users.noreply.github.com>
2017-11-20 00:53:27 +0530

    Create lab 6.Q4.cpp

---
+++ lab 6.Q4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main() {
+
+string name;
+char *p;
+cout<<"Enter your name\n";
+getline(cin,name);
+int n=name.size();
+p=&(name[0]);
+
+for(int i=0;i<n;i++)
+ { cout<<*(p+i);
+ }
+ cout<<endl;
+ for (int i=0;i<n;i++)
+  {  cout<<name[i];
+  }
+	return 0;
+
+	}
+
+	
